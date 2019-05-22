78248ba8647d92434aeda43c96deb71de116382c
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-05-22 15:23:18 -0700

    install mongoose

--- dataStore.js
+++ dataStore.js
@@ -3,3 +3,8 @@ const api_Config={
     client_id: '3n9Zq8YovnLQ0ZrRdtIiPQ',
     Authorization: 'Bearer <wnGOZBUSDSSRT1LStfPciD8pwr-BEhWAId-SegMRQkYZ5TzH5R6oeqMeCpWgYj-nnJ3FnCZbH1b9laJFTWE8TqOjcTpsb11sZ1uOEXZaGlIyFJjyBGbrlPBBg_WfXHYx>',
 }
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                      'mongodb://localhost/referrals';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                      'mongodb://localhost/referrals';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -31,12 +31,40 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// when requests come into `/shopping-list` or
-// `/recipes`, we'll route them to the express
+// when requests come into `/searchRouter` or
+// `/referralRouter`, we'll route them to the express
 // router instances we've imported. Remember,
 // these router instances act as modular, mini-express apps.
 app.use('/searchRounter', searchRouter);
-app.use('/referralRouter', referalRouter);
+app.use('/referralRouter', referralRouter);
+
+
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// `closeServer` function is here in original code
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
