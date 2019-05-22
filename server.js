2ca010514ce10cc3054cf4179633458d3f39b08e
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-05-22 11:02:22 -0700

    added some code for referral page

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
-
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const blogPostsRouter = require("./blogPostsRouter.js");
-const {} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
 
+const searchRouter = require('./searchRouter');
+const referalRouter = require('./referralRouter');
 
 // log the http layer
 app.use(morgan('common'));
-app.use(express.json());
 
-app.use("/referal-posts", referalPostsRouter);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
+// when requests come into `/shopping-list` or
+// `/recipes`, we'll route them to the express
+// router instances we've imported. Remember,
+// these router instances act as modular, mini-express apps.
+app.use('/searchRounter', searchRouter);
+app.use('/referralRouter', referalRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
