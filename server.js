8d76e58a86952d3fec095b0ff24517e18af8a253
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-05-22 12:10:52 -0700

    mongoose update

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const app = express();
 const searchRouter = require('./searchRouter');
 const referalRouter = require('./referralRouter');
 
+const mongoose = require('mongoose');
+
+// Mongoose internally uses a promise-like object,
+// but it's better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+
+// config.js is where we control constants for entire
+// app like PORT and DATABASE_URL
+const {PORT, DATABASE_URL} = require('./config');
+const {Restaurant} = require('./models');
+
 // log the http layer
 app.use(morgan('common'));
 
