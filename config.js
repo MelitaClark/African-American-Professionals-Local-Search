ae9932d16475c4550a96fbc6480c4895136654b0
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-06-04 16:31:34 -0700

    new commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                      'mongodb+srv://melita2:Loveyou2@cluster0-9rkqj.mongodb.net/createReferrals?retryWrites=true&w=majority';
+'mongodb+srv://melita2:Loveyou2@cluster0-9rkqj.mongodb.net/createReferrals?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                       'mongodb://localhost/referrals';
 exports.PORT = process.env.PORT || 8080;
