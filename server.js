02251b1669a074e5b6e77bd89d8d9d728cdabc94
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-06-18 12:01:10 -0700

    working on user authentication & access control

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 "use strict";
-
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
+const passport = require('passport');
 
 
 // Mongoose internally uses a promise-like object,
