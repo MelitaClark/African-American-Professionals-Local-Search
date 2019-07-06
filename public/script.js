8467dc2cd2df0a87e0a3e683dabf6ba53e59e665
Melita Clark <beyonce@thinkful.comgit config --global user.email lanis2boys@gmail.com>
2019-07-05 19:51:39 -0700

    updated path

--- public/script.js
+++ public/script.js
@@ -1,60 +1,52 @@
-const DATABASE_URL = 
+const DATABASE_URL = "/referrals"
+//const referrals = require('./config')
 
-function getReferrals(){
+fetch(DATABASE_URL)
+.then(response=> response.json())
+.then(data=>console.log(data))
+.then(data=>generateReferrals(data))
+
+function generateReferrals(data){
     $('.searchReferrals-button').click(event => {
         console.log('SubmitSearch Button Clicked');
         event.preventDefault();
 
-        const serviceType= $('#serviceType').val();
+       /* const serviceType= $('#serviceType').val();
          const locationType= $('#locationType').val();
          const locationCity= $('#locationCity').val();
          const locationState= $('locationState').val();
-    
-        fetch(DATABASE_URL, {
-            method: "GET",
-            
-        })
-        .then(response=>response.json())
-        .then(data=>{
-            console.log(data);
-        
-        })
-        .then(data=> handleRefs(data))
 
-    })
+         if(data.referrals.length <=0){
+            $('#message').toggleClass("hidden");
+        }else{
+            for(let i = 0; i<data.referrals.length; i++){
+                $('.search-form').remove();
+                $('#myResults').css('display,block');
+                const output = $('#myResults')
+                output.empty()
+                $('#myResults').append(`<li role="listitem class="cards_item">
+               
+                                        <div class="card">
+                                        <div class="card_content">
+                                        <h3 class="businessType">${data.referrals[i].businessType}</h3>
+                                        <h3 class="businessName">${data.referrals[i].businessName}</h3>
+                                        <h4 class= "businessInfo">Business Info:</h4>
+                                        <p> Phone Number: ${data.referrals[i].businessInfo.phone_number}</p>
+                                        <p> Email: ${data.referrals[i].businessInfo.email}</p>
+                                        <p>location: ${data.referrals[i].locaton.street} ${data.referrals[i].location.city} ${data.referrals[i].location.state} ${data.referrals[i].location.zipcode}</p>
+                                        <h4> class="businessReviews">Business Reviews:</h4>
+                                        <p> Review: ${data.referrals[i].reviews}</p>
+                                        <button class="clear-results" type="button">Clear Search Results</button>
+                                        </div>
+                                        </div>
+                                        </li>
+                                        `);
+            }*/
+        }) }
     
-}
-/*function handleRefs(data){
-    if(data.referrals.length <=0){
-        $('#message').toggleClass("hidden");
-    }else{
-        for(let i = 0; i<data.referrals.length; i++){
-            $('.search-form').remove();
-            $('#myResults').css('display,block');
-            const output = $('#myResults')
-            output.empty()
-            $('#myResults').append(`<li role="listitem class="cards_item">
-           
-                                    <div class="card">
-                                    <div class="card_content">
-                                    <h3 class="businessType">${data.referrals[i].businessType}</h3>
-                                    <h3 class="businessName">${data.referrals[i].businessName}</h3>
-                                    <h4 class= "businessInfo">Business Info:</h4>
-                                    <p> Phone Number: ${data.referrals[i].businessInfo.phone_number}</p>
-                                    <p> Email: ${data.referrals[i].businessInfo.email}</p>
-                                    <p>location: ${data.referrals[i].locaton.street} ${data.referrals[i].location.city} ${data.referrals[i].location.state} ${data.referrals[i].location.zipcode}</p>
-                                    <h4> class="businessReviews">Business Reviews:</h4>
-                                    <p> Review: ${data.referrals[i].reviews}</p>
-                                    <button class="clear-results" type="button">Clear Search Results</button>
-                                    </div>
-                                    </div>
-                                    </li>
-                                    `);
-        }
-    }
-}
+    
 
-function deleteResults(){
+/*function deleteResults(){
     $('.clear-results').on(click, event=>{
         console.log('deleted');
         location.reload();
@@ -62,11 +54,9 @@ function deleteResults(){
     })
 }
 function handleApp(){
-    getReferrals();
-    handleRefs();
+    generateReferrals();
+  
 }
 
-$(handleApp);*/
+$(handleApp); */
 
-
-getReferrals();
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
 const passport = require('passport');
-
 const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
@@ -20,12 +19,24 @@ const { PORT, DATABASE_URL } = require("./config");
 const { Referrals, USER} = require("./models");
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.use(morgan('common'));
 
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 //Code for Requests...
 
 //send a GET request for static html
@@ -184,22 +195,13 @@ app.post('/newUser', (req, res)=> {
 });
 
 
-// CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
-
+const localAuth = passport.authenticate('local', { session: false });
 
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
